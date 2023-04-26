040de7a4f3c06a08d92e21075db72fae662d8594
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-04-26 21:22:15 +0700

    Initial Commit

---
+++ AFL3_0706012110034_Marsha Alexis Likorawung_RPGBattle/AFL3_0706012110034_Marsha_Alexis_Likorawung_RPGBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110034_Marsha_Alexis_Likorawung_RPGBattleApp.swift
+//  AFL3_0706012110034_Marsha Alexis Likorawung_RPGBattle
+//
+//  Created by Marsha Likorawung  on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110034_Marsha_Alexis_Likorawung_RPGBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3_0706012110034_Marsha Alexis Likorawung_RPGBattle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3_0706012110034_Marsha Alexis Likorawung_RPGBattle
+//
+//  Created by Marsha Likorawung  on 26/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
